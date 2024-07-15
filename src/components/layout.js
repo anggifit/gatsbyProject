a7ae8b065ee6d6a84ae96b07e7eb9d7af991e000
anggifit <abonaldy@gmail.com>
2024-07-15 12:57:15 +0200

    Finished Gatsby Tutorial Part 3

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,22 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div>
       <nav className="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
         <div className="mb-2 sm:mb-0 flex flex-row">
-          <div className="h-10 w-10 self-center mr-2">
-            <img className="h-10 w-10 self-center" src="https://csscomps.com/images/csscomps.png" alt="Logo" />
+          <div className="h-20 w-20 self-center mr-2">
+            <StaticImage 
+              className="h-full w-full self-center" 
+              src="../images/icon.png" alt="Logo"/>
           </div>
           <div>
             <a href="/home" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">anggiFitCode</a><br />
             <span className="text-xs text-grey-dark">Just me</span>
           </div>
         </div>
-
         <div className="sm:mb-0 self-center">
           <Link to="/" className="text-md no-underline text-black hover:text-blue-dark ml-2 px-1">Home</Link>
           <Link to="/about" className="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1">About me</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import * as React from "react"
 import Layout from '../components/layout'
-
-import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-                  <p>I'm making this by following the Gatsby Tutorial.</p>
-
+    <Layout pageTitle="Homepage">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage class='size-1/2' alt='homepage image' src='https://images.unsplash.com/photo-1483058712412-4245e9b90334?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
     </Layout>
   )
 }
