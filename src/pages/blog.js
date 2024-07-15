2348571654b2698ada4b9e784cebeb33e67aa160
anggifit <abonaldy@gmail.com>
2024-07-15 15:42:40 +0200

    Fixed seo issues

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
-import Seo from "../components/seo";
+import Seo from "../components/Seo";
 
 const Blog = ({ data }) => {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/layout";
-import Seo from '../components/seo'
+import Seo from '../components/Seo'
 
 const IndexPage = () => {
   return (
