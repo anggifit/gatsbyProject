6670f05f99c512eb2ea55b4acaf504754792bb5d
anggifit <abonaldy@gmail.com>
2024-07-15 17:39:11 +0200

    add node version to workflow

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,19 +6,20 @@ const Blog = ({ data }) => {
   return (
     <Layout pageTitle="My nutri posts">
       <section className="relative pt-12 bg-blueGray-50">
-        <ul className='text-lg text-green-800'>
+        <ul className="text-lg text-green-800">
           {data.allFile.nodes.map((node) => (
             <li key={node.name}>{node.name}</li>
           ))}
         </ul>
       </section>
+
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile (filter: {sourceInstanceName: {eq: "blog"}}){
+    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
       nodes {
         name
       }
@@ -26,6 +27,6 @@ export const query = graphql`
   }
 `;
 
-export const Head = () => <title>My nutri posts</title>
+export const Head = () => <title>My nutri posts</title>;
 
 export default Blog;
